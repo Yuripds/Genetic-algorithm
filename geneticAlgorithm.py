37f38b35141b9ee4877cc6173fe1062cc2f0ee77
yuri pedro dos santos <Yuripds>
2022-08-09 15:22:31 -0300

    implementação das etapas de  mutação e crossover

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -1,4 +1,6 @@
 import numpy as np
+from random import randint
+from random import random
 
 
 def populationGenerator(popSize,ngenes,type,vmax=5):
@@ -19,11 +21,36 @@ def fitFunction(cromossomo):
 
     return score
 
-def selection(population,scores):
+
+### não entendi esse k igual a 2
+### como meu objetivo é maximizar uma função troquei o sinal de '<' para '>' no escopo do IF
+def selection(population,scores,k=2):
+	selection_ix = np.random.randint(len(population))
+	for ix in np.random.randint(len(population),size=k):
+		if scores[ix] > scores[selection_ix]:
+			selection_ix = ix
+	return population[selection_ix]
 
 
-    return parents
 
+def crossover(parent01,parent02,crossRate):
+    children01 = parent01.copy()
+    children02 = parent02.copy()
+    if random() > crossRate:
+        crossPoint = np.random.randint(len(parent01)-2,size=1)
+        children01 = parent01[0][:crossPoint].tolist() + parent02[0][crossPoint:].tolist()
+        children02 = parent02[0][:crossPoint].tolist()+ parent01[0][crossPoint:].tolist()
+		
+
+    return [children01, children02]
+
+
+
+def mutation(cromossomo,mutationRate):
+    for i in range(len(cromossomo)):
+        if random() > mutationRate:
+            cromossomo[i]=1-cromossomo[i]
+    return 0 
 
 def runGA(n_iter,population):
 
