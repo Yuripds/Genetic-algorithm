dc8e85600eb6ca592f035a1a13e5faf52bd1268c
Yuripds <yuripedro3@gmail.com>
2022-08-11 14:19:15 -0300

    adequação das funções custos para aplicar o AG em uma problema NOMA

--- funcaoCusto.py
+++ funcaoCusto.py
@@ -1,12 +1,14 @@
 import numpy as np
 import allocacaoPotencia as ap
 
-def dataRate(gamaUser,p_list,index):
+def dataRate(gamaUser,p_list,n_clusters):
     w =100
     B = 180*(10**3)
     N0 = 10**(-17.3)
-    Pt = (10**4.6)*(10**-3)
+    Pt = (10**4.6)*(10**-3)/n_clusters
     
+
+    #### mudar a partir daqui (mudar primeiro essa função depois a próxima)
     num = p_list[index]*Pt*gamaUser
 
     p_list = p_list[index+1:]
@@ -19,19 +21,26 @@ def dataRate(gamaUser,p_list,index):
 
     return r
 
-def sumDataRate(gamaL,alpha):
+def sumDataRate(cromossomo):
 
-    p_list = allocPower(gamaL,alpha)
-    print("p_list: ",p_list)
-    
-    r_array = np.ones((7,1))
-    r_array[:] = float(np.NaN)
-    
-    for ind in range(len(gamaL)):
-        gamaUser = gamaL[ind]
-        r = dataRate(gamaUser,p_list,ind)
-        r_array[ind] = r
+    alpha =0.3
+    n_clusters = len(np.unique(cromossomo))
 
+    gama = [] ### chamada de função de desvanecimento global 
+
+    gamaClusters = []
+    for i in np.unique(cromossomo):
+        gamaClusters.append(gama[cromossomo==i].tolist())
+
+    p_list =[ap.allocPower(i,alpha) for i in gamaClusters]
+    
+
+    data_rates=[]
+    for ix, val in enumerate(gamaClusters):
+        data_rates.append(dataRate(val,p_list[ix]),n_clusters)
+
+
+####### alterar a partir daqui
     output = r_array[np.isfinite(r_array)]
     R_global = np.sum(output)
 
